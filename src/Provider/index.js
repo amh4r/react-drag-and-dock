7b4aadb3553523d2cc70f265bd9de708cfeed8de
aharper <aharper@altair.com>
2019-01-27 13:49:50 -0500

    Move Provider logic into utils.js

---
+++ src/Provider/index.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { snapPanelToDock, upsertDock, upsertPanel } from './utils';
+import Context from '../Context';
+
+export class Provider extends Component {
+  constructor() {
+    super();
+
+    /* Seems redundant, but necessary since setState() is async. */
+    this.panels = new Map();
+    this.docks = new Map();
+
+    this.state = {
+      panels: this.panels,
+      docks: this.docks,
+    };
+  }
+
+  registerPanel = (panelRef, panelProps = {}) => {
+    this.updatePanel(panelRef, panelProps);
+  };
+
+  registerDock = (dockRef, dockProps = {}) => {
+    this.updateDock(dockRef, dockProps);
+  };
+
+  updatePanel = (panelRef, panelProps = {}) => {
+    this.panels = upsertPanel({
+      panelProps,
+      panelRef,
+      panels: this.panels,
+    });
+
+    this.setState({ panels: this.panels });
+  };
+
+  updateDock = (dockRef, dockProps = {}) => {
+    this.docks = upsertDock({
+      dockProps,
+      dockRef,
+      docks: this.docks,
+    });
+
+    this.setState({ docks: this.docks });
+  };
+
+  snapToDock = (panelRef, dockRef) => {
+    const { newDocks, newPanels } = snapPanelToDock({
+      docks: this.docks,
+      dockRef,
+      panels: this.panels,
+      panelRef,
+    });
+
+    this.docks = newDocks;
+    this.panels = newPanels;
+
+    this.setState({
+      docks: this.docks,
+      panels: this.panels,
+    });
+  };
+
+  render() {
+    const { children } = this.props;
+    const { panels, docks } = this.state;
+
+    const contextValue = {
+      panels,
+      provider: this,
+      registerPanel: this.registerPanel,
+      registerDock: this.registerDock,
+      snapToDock: this.snapToDock,
+      docks,
+      updateDock: this.updateDock,
+    };
+
+    return <Context.Provider value={contextValue}>{children}</Context.Provider>;
+  }
+}
+
+Provider.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.element, PropTypes.arrayOf(PropTypes.element)])
+    .isRequired,
+};
+
+export default Provider;
---
+++ src/Provider/utils.js
@@ -0,0 +1,98 @@
+const addPanelToDock = ({ docks, dockRef, panels, panelRef }) => {
+  const dock = docks.get(dockRef);
+  const panel = panels.get(panelRef);
+
+  const newDock = {
+    ...dock,
+    panels: new Map(dock.panels).set(panelRef, panel),
+  };
+
+  const newDocks = new Map([...docks]).set(dockRef, newDock);
+
+  return newDocks;
+};
+
+const removePanelFromDocks = ({ docks, panelRef }) => {
+  const newDocks = new Map(docks);
+
+  new Map(docks).forEach((dock, key) => {
+    const { panels } = dock;
+
+    if (panels.has(panelRef)) {
+      const newPanels = new Map(panels);
+
+      newPanels.delete(panelRef);
+
+      const newDock = {
+        ...dock,
+        panels: newPanels,
+      };
+
+      newDocks.set(key, newDock);
+    }
+  });
+
+  return newDocks;
+};
+
+const snapPanelToDock = ({ docks, dockRef, panels, panelRef }) => {
+  const panel = panels.get(panelRef);
+
+  const newPanel = {
+    ...panel,
+    snappedDock: dockRef || null,
+  };
+
+  const newPanels = new Map([...panels]).set(panelRef, newPanel);
+
+  let newDocks = removePanelFromDocks({ docks, panelRef });
+
+  newDocks = addPanelToDock({
+    docks: newDocks,
+    dockRef,
+    panels: newPanels,
+    panelRef,
+  });
+
+  return {
+    newDocks,
+    newPanels,
+  };
+};
+
+const upsertDock = ({ dockProps, dockRef, docks }) => {
+  const oldDock = docks.get(dockRef);
+
+  const newDock = {
+    panels: new Set(),
+    ...oldDock,
+    props: dockProps,
+    ref: dockRef,
+  };
+
+  const newDocks = new Map([...docks]).set(dockRef, newDock);
+
+  return newDocks;
+};
+
+const upsertPanel = ({ panelProps, panelRef, panels }) => {
+  const oldPanel = panels.get(panelRef);
+
+  const newPanel = {
+    ...oldPanel,
+    props: panelProps,
+    ref: panelRef,
+  };
+
+  const newPanels = new Map([...panels]).set(panelRef, newPanel);
+
+  return newPanels;
+};
+
+export {
+  addPanelToDock,
+  removePanelFromDocks,
+  snapPanelToDock,
+  upsertDock,
+  upsertPanel,
+};
