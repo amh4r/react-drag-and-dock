8244b79192511ed883007b24f33fe85d4dbcbdfd
aharper <aharper@altair.com>
2019-02-09 23:19:24 -0500

    Simplify area example

--- sandbox/src/examples/Area.js
+++ sandbox/src/examples/Area.js
@@ -6,27 +6,30 @@ import Timer from '../components/Timer';
 class Example extends Component {
   render() {
     return (
-      <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-        <div style={{ height: '10em' }}>yo</div>
+      <div
+        style={{
+          background: 'lightblue',
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100vh',
+        }}
+      >
+        <DragAndDock.Area>
+          <DragAndDock.Area.Center>
+            <div style={{ height: '100%', width: '100%' }}>hello</div>
+          </DragAndDock.Area.Center>
 
-        <div style={{ background: 'lightblue', flexGrow: 1 }}>
-          <DragAndDock.Area>
-            <DragAndDock.Area.Center>
-              <div style={{ height: '100%', width: '100%' }}>hello</div>
-            </DragAndDock.Area.Center>
+          <DragAndDock.Area.Dock location="left" width={300} />
+          <DragAndDock.Area.Dock location="right" width={300} />
 
-            <DragAndDock.Area.Dock location="left" width={300} />
-            <DragAndDock.Area.Dock location="right" width={300} />
+          <DragAndDock.Area.Panel title="Panel 1" initialDockUid="left">
+            <div>I am panel 1</div>
+          </DragAndDock.Area.Panel>
 
-            <DragAndDock.Area.Panel title="Panel 1" initialDockUid="left">
-              <div>I am panel 1</div>
-            </DragAndDock.Area.Panel>
-
-            <DragAndDock.Area.Panel title="Panel 2">
-              <Timer />
-            </DragAndDock.Area.Panel>
-          </DragAndDock.Area>
-        </div>
+          <DragAndDock.Area.Panel title="Panel 2">
+            <Timer />
+          </DragAndDock.Area.Panel>
+        </DragAndDock.Area>
       </div>
     );
   }
